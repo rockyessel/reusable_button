2cfc5e99fa4c353826007a3e4a96b16ff79c661c
Rocky Essel <rockyessel76@gmail.com>
2023-04-24 10:04:08 +0000

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import Button from './components/button';
 
 const App = () => {
-  return <main>App</main>;
+  return (
+    <main className='bg-black h-screen text-gray-50 flex items-center justify-center'>
+      <Button
+        handleClick={() => {}}
+        styles={'w-fit bg-violet-500'}
+        type={'button'}
+        title={'Open modal'}
+        disabled={false}
+      >
+        {/* children */}
+        Open modal
+      </Button>
+    </main>
+  );
 };
 
 export default App;
